9db2c7fc8729b8bbd29b3786e08e2f21be9bc769
Normie69K <157805964+Normie69K@users.noreply.github.com>
2026-09-09 14:07:05 +0530

    feat(pipeline): add parse_args to report_gen

--- pipeline/report_gen.py
+++ pipeline/report_gen.py
@@ -14,3 +14,13 @@ try:
 except ImportError:
     pass
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="SecureForge Report Generator")
+    parser.add_argument('--input', required=True, help="Input JSON file")
+    parser.add_argument('--template', required=True, choices=['erasure', 'recovery', 'custody'])
+    parser.add_argument('--output', required=True, help="Output PDF path")
+    parser.add_argument('--investigator', help="Investigator name")
+    parser.add_argument('--case-id', help="Case ID")
+    parser.add_argument('--tsa-token', help="TSA token path")
+    return parser.parse_args()
+
